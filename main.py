096ad1e0a13d426e06e83b7fdd0d2e1a3675ee1b
Prabhakar Kulkarni <kulkarniprabhakar2000@gmail.com>
2022-04-22 19:26:02 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+from twilio.rest import Client
+
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = "b97ec3ce008c40c8b438fe5b36b4c2b5"
+account_sid = "Your twillo account id"
+auth_token = "your twillo auth token"
+
+# The below parameters are taken from the API parameters given already
+weather_params = {
+    "lat":31.230391,                 #17.914881,
+    "lon":121.473701,                           #77.504608,
+    "appid":api_key,
+    "exclude":"current,minutely,daily"
+}
+response = requests.get(OWM_Endpoint, params=weather_params)
+# print(response.cookies)
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["hourly"][0]["weather"][0])
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = False
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an Umbrella ☔",
+        from_="from no",
+        to="to no"
+    )
+    print(message.status)
+else:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="No need of an Umbrella =  ☔. No rain!!",
+        from_="from no",
+        to="to no"
+    )
+    print(message.status)
